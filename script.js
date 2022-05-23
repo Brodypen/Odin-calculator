78023450574aa25c9cc1718a1e5c976cb5a9dea4
BrodyLinux <Frostwizard7987@gmail.com>
2022-05-23 10:47:45 -0500

    Operators and operants!

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 let equationValue = "";
-let numToEvalValue = "";
-
+let numToEvalValue = "0";
+let SecnumToEvalValue = "0";
+let operation = "";
+let notValid = false;
 // DOM elements
 equation = document.querySelector(".equation");
 numToEval = document.querySelector(".numToEval");
-
+clearButton = document.querySelector(".clear");
+clearButton.addEventListener("click", () => {
+  clear();
+});
 calNumber = document.querySelectorAll(".calNumber");
 calNumber = [...calNumber];
 calNumber.forEach((element) => {
@@ -12,12 +17,99 @@ calNumber.forEach((element) => {
     calNumberToEval(element);
   });
 });
-function calNumberToEval(elem) {
-  
-  numToEvalValue += elem.getAttribute("data-value");
-  console.log(numToEvalValue);
-  numToEval.innerHTML = numToEvalValue;
+
+operator = document.querySelectorAll(".operator");
+operator = [...operator];
+operator.forEach((element) => {
+  element.addEventListener("click", () => {
+    operatorToEval(element);
+  });
+});
+function backspace(){
+  if(numToEvalValue.length > 1){
+  numToEvalValue = numToEvalValue.substring(0,numToEvalValue.length - 1);
+  display();  
+  } else {
+    numToEvalValue = "0";
+    display();
+  }
+
 }
+function calNumberToEval(elem) {
+  if (elem.getAttribute("data-value") === "." && numToEvalValue.includes("."))
+    return;
+  if (numToEvalValue === "0") {
+    numToEvalValue = elem.getAttribute("data-value");
+  } else {
+    numToEvalValue += elem.getAttribute("data-value");
+  }
+  display();
+}
+function operatorToEval(elem) {
+  if (numToEvalValue !== ".") {
+    if (operation === "") {
+      operation = elem.getAttribute("data-value");
+      if(operation === "switch"){
+        switchSigns();
+        operation = "";
+      } else{
+      
+      SecnumToEvalValue = numToEvalValue;
+      numToEvalValue = "0";
+      }
+      display();
+    } else {
+      calculate();
+      operatorToEval(elem);
+    }
+  }
+}
+function switchSigns(){
+  numToEvalValue = -1 * parseFloat(numToEvalValue);
+}
+function calculate() {
+  let ans;
+  const prev = parseFloat(SecnumToEvalValue);
+  const current = parseFloat(numToEvalValue);
+  if (isNaN(prev) || isNaN(current) || operation == '') return;
+  switch (operation) {
+    case "+":
+      ans = add(prev, current);
+      break;
+
+    case "-":
+      ans = minus(prev, current);
+      break;
+    case "*":
+      ans = multiply(prev, current);
+      break;
+    case "÷":
+      ans = divide(prev, current);
+      break;
+    case "^":
+      ans = pow(prev, current);
+      break;
+  }
+  SecnumToEvalValue += " " + operation + " " + numToEvalValue + " =";
+  numToEvalValue = ans;
+  operation = "";
+  display();
+}
+function display() {
+  numToEval.innerHTML = numToEvalValue;
+  equation.innerHTML = SecnumToEvalValue + " " + operation;
+}
+function clear() {
+  if (numToEvalValue != "0") {
+    numToEvalValue = "0";
+    display();
+    return;
+  }
+  operation = "";
+  SecnumToEvalValue = "0";
+  display();
+}
+
 // All functions for calcualting
 function add(a, b) {
   return a + b;
@@ -29,11 +121,11 @@ function multiply(a, b) {
   return a * b;
 }
 function divide(a, b) {
+  if(b === 0){
+    return "ERORR!";
+  }
   return a / b;
 }
 function pow(a, b) {
   return Math.pow(a, b);
-}
-function sqrt(a) {
-  return Math.sqrt(a);
 }

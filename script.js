7937c2e722e129aefac694110c17321f1bad240b
BrodyLinux <Frostwizard7987@gmail.com>
2022-05-22 19:58:27 -0500

    Add all mathmatical javascript code

---
+++ script.js
@@ -0,0 +1,18 @@
+function add(a, b) {
+  return a + b;
+}
+function minus(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  return a / b;
+}
+function pow(a,b){
+    return Math.pow(a,b);
+}
+function sqrt(a){
+  return Math.sqrt(a);
+}
